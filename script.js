59831cd93b00b3deb349e63aa82bc3b193e55992
Gregory Holder <gregory.holder76@gmail.com>
2022-04-07 09:36:23 +0200

    Implement Corrections

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const observer = new IntersectionObserver(entries => {
     // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('animation');
-        // console.log("hi")
-      }
-    });
+    entries.forEach(entry => 
+      entry.isIntersecting && entry.target.classList.add('animation')
+    );
 });
 
-document.querySelectorAll('.percent').forEach((element => {
-    observer.observe(element);
-}))
-
+document.querySelectorAll('.percent').forEach((element => observer.observe(element)))
 observer.observe(document.querySelector(".imgBox"))
